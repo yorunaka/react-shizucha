a1ff640365a82d63f69d66e67e9a1ee491746b62
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-08-07 13:36:31 +0700

    Initial Commit

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className='overflow-x-hidden'>
-      <Hero/>
+      <Hero />
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,16 +1,27 @@
-import React, { Fragment } from 'react'
-import BgImage from '../../assets/images/bg.jpg'
+import React from 'react'
+import background from ''
 
 const Hero = () => {
   return (
-  <main className='bgImage'>
-    <section>
+  <main style={{
+    backgroundImage: `url(${background})`,
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    backgroundPosition: 'center',
+    height: '100vh'
+  }}>
+    <section className="min-h-[750px] w-full">
         <div className="container">
         {/* Navbar Section */}
+
+        {/* Hero Section */}
+          <div className="grid grid-cols-1 md:grid-cols-2">
+
+          </div>
         </div>
     </section>
   </main>
-  )
-}
+  );
+};
 
 export default Hero
