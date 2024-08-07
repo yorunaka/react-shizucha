49a49e6c7876f1581af4098feacf5f715d9c076d
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-08-07 12:37:03 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='overflow-x-hidden'>
+      <Hero/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/assets/components/Hero/Hero.jsx
@@ -0,0 +1,15 @@
+import React, { Fragment } from 'react'
+
+const Hero = () => {
+  return (
+  <main className='bgImage'>
+    <section>
+        <div className="container">
+        {/* Navbar Section */}
+        </div>
+    </section>
+  </main>
+  )
+}
+
+export default Hero
