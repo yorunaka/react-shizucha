395cff681ade5e21f6ed271b656632eff3c2ad52
Vynitial <109939562+yorunaka@users.noreply.github.com>
2024-08-07 13:56:13 +0700

    Initial Commit

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,22 +1,27 @@
 import React from 'react'
-import background from ''
+import background from '../../assets/bg.jpg'
+
+const bgimg= {
+  backgroundImage: `url(${background})`,
+  backgroundSize: 'cover',
+  backgroundPosition: 'center',
+  backgroundRepeat: 'no-repeat',
+}
 
 const Hero = () => {
   return (
-  <main style={{
-    backgroundImage: `url(${background})`,
-    backgroundSize: 'cover',
-    backgroundRepeat: 'no-repeat',
-    backgroundPosition: 'center',
-    height: '100vh'
-  }}>
+  <main style={bgimg}>
     <section className="min-h-[750px] w-full">
         <div className="container">
         {/* Navbar Section */}
 
         {/* Hero Section */}
           <div className="grid grid-cols-1 md:grid-cols-2">
-
+            <div>
+              <h1>
+                Shizucha
+              </h1>
+            </div>
           </div>
         </div>
     </section>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,17 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      container: {
+        center: true,
+        padding: {
+          DEFAULT: '1rem',
+          sm: '2rem',
+          lg: '4rem',
+          xl: '5rem',
+        },
+      },
+    },
   },
   plugins: [],
 }
