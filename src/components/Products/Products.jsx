d77eb9a0f715eea3f884f8ef2bc21fbab6bdb590
yorunaka <vincent.fernando12345@gmail.com>
2024-08-14 16:27:46 +0700

    Urgent Commit

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React from 'react'
 
 const Product = () => {
@@ -10,12 +11,16 @@ const Product = () => {
       <div className='grid grid-cols-3 p-6'>
         {/* produk 1 */}
         <div className='bg-yellow-600/30 p-3 rounded-md'>
+        <div className='pt-2 pb-4 text-center font-bold text-xl '>
           <h1>Matcha</h1>
-          <div className=''>
+        </div>
+          <div className='grid items-center h-[100px] w-[100px]'>
             <img src="src/assets/product-1.png" alt="" className='rounded-xl'/>
           </div>
+          <div className='pt-2 pl-2'>
           <p className=''>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam dolore expedita voluptates voluptate error mollitia earum magnam eveniet labore nemo!</p>
-          <div className='p-3'>
+          </div>
+          <div className='pt-3 pb-3 pl-2'>
           <a href="#" className='button nline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-yellow-700 rounded-lg'>Order Here!</a>
           </div>
         </div>
